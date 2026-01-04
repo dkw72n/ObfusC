5246c406b9eb000d1cea3019515a576082696a83
dkw72n <dkw72n@k70p>
2026-01-04 08:46:55 +0000

    atd: 增加变化

--- source/ATD/AtdPass.cpp
+++ source/ATD/AtdPass.cpp
@@ -1,6 +1,65 @@
 #include "AtdPass.hpp"
+#include <format>
+
+namespace atd::detail {
+    std::string rand_byte_hex(){
+        return std::format("{:#04x}", rng() & 0xff);
+    }
+    llvm::InlineAsm* GenFakeRet(llvm::Module& mod){
+        auto VoidFT = llvm::FunctionType::get(llvm::Type::getVoidTy(mod.getContext()), false);
+        auto triple = mod.getTargetTriple();
+        if (triple.find("x86_64") != std::string::npos){
+            switch (rng() % 2){
+                case 0: {
+                    std::string code = R"asm(
+                            lea  0f(%rip), %rax
+                            push %rax
+                            ret
+                        .byte )asm";
+                    auto length = rng() % 15 + 1;
+                    while(length){
+                        length--;
+                        code += rand_byte_hex();
+                        if (length){
+                            code += ",";
+                        } else {
+                            code += "\n";
+                        }
+                    }
+                    code += "0:\n";
+                    return llvm::InlineAsm::get(VoidFT, code, "~{rax}", true /*hasSideEffects*/, false);
+                }
+                case 1: {
+                    std::string code = R"asm(
+                            call 1f
+                            .byte )asm"; 
+                    auto length = rng() % 15 + 1;
+                    while(length){
+                        length--;
+                        code += rand_byte_hex();
+                        if (length){
+                            code += ",";
+                        } else {
+                            code += "\n";
+                        }
+                    }
+                    code += R"asm(
+                            ret
+                    1:
+                            pop %rax
+                    )asm";
+                    return llvm::InlineAsm::get(VoidFT, code, "~{rax}", true /*hasSideEffects*/, false);
+                }
+            }
+        }
+        return llvm::InlineAsm::get(VoidFT, R"asm(
+                    nop
+        )asm", "", true /*hasSideEffects*/, false);
+    }
+}
 
 namespace obfusc {
+    
     
     AtdPass::AtdPass(): M(nullptr), _fake_ret(nullptr){}
     AtdPass::~AtdPass() {}
@@ -15,9 +74,11 @@ namespace obfusc {
         // llvm::outs() << "[-] target: " << mod.getContext().getDefaultTargetCPU() << "\n";
         if (triple.find("x86_64") != std::string::npos){
             _fake_ret = llvm::InlineAsm::get(VoidFT, R"asm(
-                lea 2(%rip), %rax
+                lea  0f(%rip), %rax
                 push %rax
                 ret
+            .byte 0xc3, 0xc4, 0xc5, 0xc6, 0x19, 0x90, 0x07, 0x24
+            0:
             )asm", "~{rax}", true /*hasSideEffects*/, false);
             llvm::outs() << "[-] created x86-64 _fake_ret: " << *_fake_ret << "\n"; 
         }
@@ -46,9 +107,9 @@ namespace obfusc {
             if (rng() % 15 <= 1){
                 llvm::IRBuilder<> IRB(It);
                 llvm::outs() << "[-] [ATD] Inserting: " << func.getName() << "@" << *It << "\n";
-                IRB.CreateCall(_fake_ret->getFunctionType(), _fake_ret);
+                auto fake_ret = atd::detail::GenFakeRet(mod);
+                IRB.CreateCall(fake_ret->getFunctionType(), fake_ret);
                 changed |= true;
-                
                 // break;
             }
             
